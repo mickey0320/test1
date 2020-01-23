4142753843c489e35c1e142fed3edc627e935555
yanjian <yanjian92@126.com>
2020-01-24 00:01:35 +0800

    update

--- shipitfile.js
+++ shipitfile.js
@@ -2,10 +2,24 @@ module.exports = shipit => {
   // Load shipit-deploy tasks
   require('shipit-deploy')(shipit)
 
+  // shipit.task('deploy', async () => {
+  // shipit.on('')
+  // await shipit.remote('pm2 startOrRestart /var/www/pojects/current/ecosystem.json')
+  // })
+  shipit.on('fetched', () => {
+    shipit.start('app:build');
+  })
+
+  shipit.blTask('app:build', async () => {
+    // await shipit.remote('cd /var/www/pojects/current; npm install');
+    // await shipit.remote('npm install');
+    await shipit.remote('sh test.sh');
+  })
+
   shipit.initConfig({
     default: {
       deployTo: '/var/www/pojects',
-      repositoryUrl: 'https://github.com/mickey0320/test.git',
+      repositoryUrl: 'git://github.com/mickey0320/test.git',
     },
     production: {
       servers: 'root@106.12.202.39:10022'
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/www/pojects/current
+npm install
