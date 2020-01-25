afee7cdba20036e14d393507bda8c46bd5d16677
yanjian <yanjian92@126.com>
2020-01-25 15:25:14 +0800

    update

--- shipitfile.js
+++ shipitfile.js
@@ -1,27 +1,22 @@
 module.exports = shipit => {
   // Load shipit-deploy tasks
   require('shipit-deploy')(shipit)
-
-  // shipit.task('deploy', async () => {
-  // shipit.on('')
-  // await shipit.remote('pm2 startOrRestart /var/www/pojects/current/ecosystem.json')
-  // })
-  shipit.on('published', () => {
-    return shipit.start('install');
-  })
-
-  shipit.blTask('install', async () => {
-    var path = shipit.currentPath;
-
-    return shipit.remote('echo $PATH && cd ' + path + ' && npm install');
-  })
+  require('shipit-pm2')(shipit);
 
   shipit.initConfig({
     default: {
-      workspace: '/User/jianyan/shipit-workspace/',
-      deployTo: '/var/www/projects',
-      repositoryUrl: 'git://github.com/mickey0320/test.git',
-      shallowClone: true
+      workspace: '/Users/jianyan/shipit-workspace',
+      deployTo: '/var/www/project/test',
+      repositoryUrl: 'git://github.com/mickey0320/test1.git',
+      shallowClone: false,
+      pm2: {
+        json: '/var/www/project/test/current/ecosystem.json'
+      },
+      npm: {
+        remote: true,
+        // installArgs: ['gulp'],
+        // installFlags: ['-g']
+      }
     },
     production: {
       servers: 'root@106.12.202.39:10022'
